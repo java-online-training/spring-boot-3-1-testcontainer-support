c2997cb148977eb394f872bc56e213f853a4a81d
Ernesto Elias-Nieland <superernie77@gmail.com>
2023-07-30 13:17:13 +0200

    changes class names

---
+++ src/test/java/com/jot/springboot31testcontainersupport/BookEnpointIntegrationTest.java
@@ -0,0 +1,34 @@
+package com.jot.springboot31testcontainersupport;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.web.client.RestTemplate;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest(  webEnvironment = WebEnvironment.RANDOM_PORT ) 
+@Testcontainers
+class SpringBoot31TestcontainerSupportApplicationTests {
+
+	@LocalServerPort
+    int randomServerPort;
+
+	@ServiceConnection
+	@Container
+	static MongoDBContainer container = new MongoDBContainer("mongo:latest");
+
+	@Test
+	public void testBookEndpoint() {
+
+		RestTemplate template = new RestTemplate();
+
+		Book[] books = template.getForObject("http://localhost:"+randomServerPort+"/books", Book[].class);
+
+		Assert.assertEquals(3, books.length);
+	}
+}
--- src/test/java/com/jot/springboot31testcontainersupport/TestApplicationStarter.java
+++ src/test/java/com/jot/springboot31testcontainersupport/TestApplicationStarter.java
@@ -4,11 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
-import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.MongoDBContainer;
 
 @TestConfiguration(proxyBeanMethods = false)
-public class TestSpringBoot31TestcontainerSupportApplication {
+public class TestApplicationStarter {
 
 	@Bean
 	@ServiceConnection
@@ -17,7 +16,7 @@ public class TestSpringBoot31TestcontainerSupportApplication {
 	}
 
 	public static void main(String[] args) {
-		SpringApplication.from(SpringBoot31TestcontainerSupportApplication::main).with(TestSpringBoot31TestcontainerSupportApplication.class).run(args);
+		SpringApplication.from(ApplicationStarter::main).with(TestApplicationStarter.class).run(args);
 	}
 
 }
